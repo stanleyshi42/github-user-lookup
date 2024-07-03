d8df96016f1d30d85670cacee4358fc815782d6e
stanleyshi42 <stanleyshi42@gmail.com>
2024-07-03 06:53:47 -0700

    implemented user lookup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'github-user-lookup';
+  user: any;
+  username = '';
+
+  constructor(private service: GithubService) {}
+
+  getUser(username: string) {
+    this.user = this.service.getUser(username).subscribe((data) => {
+      console.log(data);
+      this.user = data;
+    });
+  }
 }
